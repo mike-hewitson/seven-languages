8346f717eea730f061af52af6efb045070f589b3
Nico Swanepoel <icnico199@gmail.com>
2016-08-29 04:31:36 +0200

    Removed extra lines from the file

--- erlang/translate_service.erl
+++ erlang/translate_service.erl
@@ -13,16 +13,13 @@ loop() ->
     receive
         {From, "casa"} -> 
             From ! "house", 
-            loop();
-        
-		
+            loop();    
         {From, "blanca"} -> 
             From ! "white", 
-            loop();
-        
+            loop(); 
         {From, _} -> 
             From ! "I don't understand.", 
-            loop()       
+            loop()			
 end.
 
 translate(To, Word) ->
